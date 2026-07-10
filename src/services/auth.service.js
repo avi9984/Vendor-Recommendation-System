57d398d98aa339dbcc55384de493801c98371c2e
avi9984 <avinashkumar151199@gmail.com>
2026-07-10 15:05:54 +0530

    auth services more flexible changes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -12,15 +12,35 @@ export const register = async (payload) => {
         throw ApiError(409, "Email already exists")
     }
 
+    // Check if there is an existing vendor profile created by Admin for this email
+    const existingVendor = await prisma.vendor.findUnique({
+        where: { email: payload.email }
+    })
+
+    let role = payload.role;
+    if (existingVendor) {
+        role = "VENDOR";
+    }
+
     const hashPassword = await bcrypt.hash(payload.password, 10)
 
-    return prisma.user.create({
+    const newUser = await prisma.user.create({
         data: {
             ...payload,
+            role,
             password: hashPassword
         }
     })
 
+    // If vendor exists, link it to the newly created user
+    if (existingVendor) {
+        await prisma.vendor.update({
+            where: { id: existingVendor.id },
+            data: { userId: newUser.id }
+        })
+    }
+
+    return newUser;
 }
 
 export const login = async (payload) => {
