0c924f84c74b82e9ba039eb7bf9a2b214a367b5e
avi9984 <avinashkumar151199@gmail.com>
2026-07-10 15:02:25 +0530

    work requirement feature done

--- src/routes/workRequirement.routes.js
+++ src/routes/workRequirement.routes.js
@@ -2,18 +2,22 @@ import { Router } from "express";
 import * as controller from "../controllers/workRequirement.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { createWorkRequirementSchema, updateWorkRequirementSchema } from "../validators/workRequirement.validator.js";
+import authMiddleware from "../middlewares/auth.middlewares.js";
+import { authorizeRoles } from "../middlewares/authorization.middlewares.js";
+import { UserRoles } from "../config/constants.js";
 
 const router = Router();
 
-router.post("/", validate(createWorkRequirementSchema), controller.createWorkRequirement)
+router.use(authMiddleware);
+router.post("/", authorizeRoles(UserRoles.USER, UserRoles.ADMIN), validate(createWorkRequirementSchema), controller.createWorkRequirement)
 
 router.get("/", controller.getAllWorkRequirements)
 
 router.get("/:id", controller.getWorkRequirementById)
 
-router.put("/:id", validate(updateWorkRequirementSchema), controller.updateWorkRequirement)
+router.put("/:id", authorizeRoles(UserRoles.USER, UserRoles.ADMIN), validate(updateWorkRequirementSchema), controller.updateWorkRequirement)
 
-router.delete("/:id", controller.deleteWorkRequirement)
+router.delete("/:id", authorizeRoles(UserRoles.USER, UserRoles.ADMIN), controller.deleteWorkRequirement)
 
 router.get("/:id/recommendations", controller.getRecommendedVendor)
 
